0bbdf61b0f06a8375848c3675b20a5d8ff2b5f52
prakunj <prakunjchaudhary@gmail.com>
2024-07-30 09:25:19 +0300

    initial commit

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     sepolia: {
       url: 'https://eth-sepolia.g.alchemy.com/v2/wOPXjUkG_5u4VOU-JN85HD3KvvvtEEvo',
-      accounts: ['1b2a212ac1448652ec151f3dc7ea8820b43bc73346bad2a6088196e67bc7d1e9'],
+      accounts: [''],
     },
   },
 };
